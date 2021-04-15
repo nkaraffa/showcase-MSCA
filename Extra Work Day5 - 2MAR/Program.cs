1edb001142db56b950987d7011cf5b881158de7b
nkaraffa <81177008+nkaraffa@users.noreply.github.com>
2021-04-15 13:47:50 -0700

    Add files via upload

---
+++ Extra Work Day5 - 2MAR/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extra_Work_Day5___2MAR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome. Please select an option to compute area:");
+            int user = 0;
+
+            while (user != 4)
+            {
+                Console.WriteLine("1: Circle   2: Square  3: Color  4: Exit");
+                try
+                {
+                    user = int.Parse(Console.ReadLine());
+
+                    switch (user)
+                    {
+                        case 1:
+                            calcCircle();
+                            break;
+                        case 2:
+                            calcSquare();
+                            break;
+                        case 3:
+                            colorShape();
+                            break;
+                        case 4:
+                            Console.WriteLine("Thank you for coming");
+                            user = 4;
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }                            
+            }
+        }
+
+        //Method that creates a circle then calculates area
+        static void calcCircle()
+        {
+            Circle circle1 = new Circle();
+            Console.WriteLine("Input circle radius:");
+            try
+            {
+                circle1._radius = double.Parse(Console.ReadLine());
+                double area1 = circle1.CalcArea(circle1._radius);
+                Console.WriteLine("The circle with radius {0} has area of {1}", circle1._radius, area1);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                GC.Collect();
+            }            
+        }
+
+        //Method that creates a square then calculates area
+        static void calcSquare()
+        {
+            Square square1 = new Square();
+            Console.WriteLine("Input square side length:");
+            try
+            {
+                square1._length = double.Parse(Console.ReadLine());
+                double area2 = square1.CalcArea(square1._length);
+                Console.WriteLine("The square with length {0} has area of {1}", square1._length, area2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                GC.Collect();
+            }            
+        }
+
+        //Method that creates a circle or square then colors it.
+        static void colorShape()
+        {
+            int userSel = 0;
+            Console.WriteLine("Select a shape:  1: Circle  2: Square");
+            try 
+            { 
+                userSel = int.Parse(Console.ReadLine()); 
+
+                if (userSel == 1)
+                {
+                    Circle circleA = new Circle();
+                    Console.WriteLine("Type in your color below");
+                    circleA._color = Console.ReadLine();
+                    Console.WriteLine("Your circle is now the color {0}", circleA._color);
+                }
+                else if (userSel == 2)
+                {
+                    Square squareA = new Square();
+                    Console.WriteLine("Type in your color below");
+                    squareA._color = Console.ReadLine();
+                    Console.WriteLine("Your square is now the color {0}", squareA._color);
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect Selection.");
+                    return;
+                }            
+            }
+            catch (Exception ex) 
+            { 
+                Console.WriteLine(ex.Message); 
+            }
+            finally
+            {
+                GC.Collect();
+            }          
+        }
+ 
+    }
+}
---
+++ Extra Work Day5 - 2MAR/Shapes.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extra_Work_Day5___2MAR
+{
+    //Base Class: Shape
+    class Shapes
+    {
+        private int id;
+        private string name;
+        private string description;
+        private string color;
+
+        public int _id
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+
+        public string _name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public string _desription
+        {
+            get { return this.description; }
+            set { this.description = value; }
+        }
+
+        public string _color
+        {
+            get { return this.color; }
+            set { this.color = value; }
+        }
+
+        public virtual double CalcArea(double length)
+        {
+            return length;
+        }
+    }
+
+    //Sub-Class: Circle
+    class Circle:Shapes
+    {
+        private double radius;
+
+        public double _radius
+        {
+            get { return this.radius; }
+            set { this.radius = value; }
+        }
+
+        //Constructors (Blank & Filled in)
+        public Circle()
+        {
+
+        }
+        public Circle(int id, string name, string descrip, string color, double radius)
+        {
+            this._id = id;
+            this._name = name;
+            this._desription = _desription;
+            this._color = color;
+            this.radius = radius;
+        }
+
+        //Calculates area of circles
+        public override double CalcArea(double length)
+        {
+            double area = 0;
+            area = 3.14159 * (length * length);
+            return area;
+        }
+    }
+
+    //Sub-Class: Square
+    class Square : Shapes
+    {
+        private double length;
+
+        public double _length
+        {
+            get { return this.length; }
+            set { this.length = value; }
+        }
+
+        //Constructors (Blank & Filled in)
+        public Square()
+        {
+
+        }
+        public Square(int id, string name, string descrip, string color, double length)
+        {
+            this._id = id;
+            this._name = name;
+            this._desription = _desription;
+            this._color = color;
+            this.length = length;
+        }
+
+        //Calculates area of squares
+        public override double CalcArea(double length)
+        {
+            double area = 0;
+            area = (length * length);
+            return area;
+        }
+    }
+}
